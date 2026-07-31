80209032dac459118b56b24292e3c6d58c4dc450
Wenje Ma <wenjema6@gmail.com>
2026-07-31 19:09:50 +0800

    ipynb转md

---
+++ convert.py
@@ -0,0 +1,36 @@
+import nbformat
+
+# ====================
+# 改这里!!!
+# ====================
+
+with open(r'c:\Users\18904\Github\Wenje-Stats\ED4DSE\02-figures\Ch00\0.0\0.0.ipynb', 'r', encoding='utf-8') as f:
+    content = f.read()
+start = content.index('{')
+end = content.rindex('}') + 1
+nb = nbformat.reads(content[start:end], as_version=4)
+output = []
+for cell in nb.cells:
+    if cell.cell_type == 'markdown':
+        output.append(cell.source)
+    elif cell.cell_type == 'code':
+        if cell.source.strip():
+            
+            # ====================
+            # # 改这里!!!
+            # # ====================
+
+            output.append('```r')
+            output.append(cell.source)
+            output.append('```')
+
+md_content = '\n\n'.join(output)
+
+# ====================
+# 改这里!!!
+# ====================
+
+with open(r'c:\Users\18904\Github\Wenje-Stats\ED4DSE\02-figures\Ch00\0.0\0.0.md', 'w', encoding='utf-8') as f:
+    f.write(md_content)
+
+print('转换完成!')
